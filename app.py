31368abae97ed2dcac12208889072ad7aab92b74
Fernando <medeirosfernandoc@gmail.com>
2025-08-01 10:41:59 -0300

    new features

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, Campeonato_grupos
+from datetime import datetime
 
 app = Flask(__name__)
+
 app.secret_key = 'sua_chave_secreta_aqui'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -58,9 +61,32 @@ def dashboard():
 def grupos_form():
     return render_template('grupos_form.html')
 
+@app.route('/criar_campeonato_grupos', methods=['POST'])
+def criar_campeonato_grupo():
+    try:
+        total_equipes = int(request.form['total_equipes'])
+        equipes_por_grupo = int(request.form['equipes_por_grupo'])
+        classificados_por_grupo = int(request.form['classificados_por_grupo'])
+
+        novo_campeonato = Campeonato_grupos(
+            total_equipes=total_equipes,
+            equipes_por_grupo=equipes_por_grupo,
+            classificados_por_grupo=classificados_por_grupo
+        )
+        db.session.add(novo_campeonato)
+        db.session.commit()
+
+        # Redirecionar para a página de edição de nomes das equipes
+        return redirect(url_for('editar_equipe_nomes', campeonato_id=novo_campeonato.id))
+
+    except Exception as e:
+        return f"Ocorreu um erro ao criar o campeonato: {e}", 500
+
 @app.route('/eliminatoria_form')
 def eliminatoria_form():
     return render_template('eliminatoria_form.html')
+
+
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -6,3 +7,11 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     senha = db.Column(db.String(200), nullable=False)
+
+class Campeonato_grupos(db.Model):  # Nome da classe corrigido para coincidir com a convenção usada
+    id = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(50), default='grupos')  # Valor padrão pode ser útil
+    total_equipes = db.Column(db.Integer, nullable=False)
+    equipes_por_grupo = db.Column(db.Integer, nullable=False)
+    classificados_por_grupo = db.Column(db.Integer, nullable=False)
+    criado_em = db.Column(db.DateTime, default=datetime.utcnow)
